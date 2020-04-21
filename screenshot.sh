5ae4ac52e85a11c7ea6f3f39a061990be99849ab
Michele Sorcinelli <michelesr@autistici.org>
2020-04-21 17:34:07 +0100

    Add screenshot script

---
+++ screenshot.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# adapted from: https://legacy.sr.ht/EJ5C.sh
+output_file="/tmp/screenshot-$(date '+%F-%T')"
+
+case "$1" in
+	"full")
+		grim $output_file ;;
+	"rect")
+		grim -g "$(slurp)" $output_file ;;
+	"active_window")
+		grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible? and .focused?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')" $output_file ;;
+esac
