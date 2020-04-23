fcc7bf96a8e51733daf66244697baacb4f71b7f8
Michele Sorcinelli <michelesr@autistici.org>
2020-04-23 15:35:09 +0100

    Run swaylock.sh in background
    To emulate -f flag in swaylock

--- swaylock.sh
+++ swaylock.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-# wrap swaylock so that sway will close the session
-# if swaylocks exits with status code not in
-#   0: success
-#   2: screen already locked
+# Protect sway from swaylock accidental termination
+#
+# Args:
+#   -f | --foreground : run in foreground
+CMD='swaylock -c 000000'
 
-# handle termination signals: SIGKILL can't be handled
-trap 'swaymsg exit && exit 0' SIGTERM SIGINT SIGQUIT SIGHUP
+lock() {
+  # handle termination signals: SIGKILL can't be handled
+  trap 'swaymsg exit && exit 0' SIGTERM SIGINT SIGQUIT SIGHUP
 
-lock='swaylock -c 000000'
-$lock &
-wait %1
-case "$?" in
-  "0"|"2")
-    exit 0 ;;
-  *)
-    swaymsg exit ;;
-esac
+  $CMD &
+  wait %1
+  case "$?" in
+    "0"|"2")
+      exit 0 ;;
+    *)
+      swaymsg exit ;;
+  esac
+}
+
+if [[ "$1" == '-f' || "$1" == '--foreground' ]]; then
+  lock
+else
+  lock &
+  disown
+  exit 0
+fi
